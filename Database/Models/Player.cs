468cfb240d35da28711afba67ca1ba5811aade67
AlaaGh1998 <159731472+AlaaGh1998@users.noreply.github.com>
2026-03-23 11:23:40 +0200

    Created Player model

---
+++ Database/Models/Player.cs
@@ -0,0 +1,7 @@
+public class Player
+{
+    public int Id { get; set; }
+    public string Username { get; set; }
+    public string PasswordHash { get; set; }
+    public string Email { get; set; }
+}
