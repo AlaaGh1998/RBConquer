4f85f8b2d9c9292879d66d0cb9a4bfe88d7ad294
AlaaGh1998 <159731472+AlaaGh1998@users.noreply.github.com>
2026-03-23 11:23:50 +0200

    Adding GameEngine header file

---
+++ src/GameEngine/GameEngine.h
@@ -0,0 +1,13 @@
+#ifndef GAME_ENGINE_H
+#define GAME_ENGINE_H
+
+class GameEngine {
+public:
+    GameEngine();
+    ~GameEngine();
+    void Initialize();
+    void Update();
+    void Render();
+};
+
+#endif // GAME_ENGINE_H
